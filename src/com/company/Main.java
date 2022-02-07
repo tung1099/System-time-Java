cfd784fb638d3ada8a5253ab2f0f575b68b06731
GnuT 268 <dtroyal1102@gmail.com>
2022-02-07 20:51:12 +0700

    System Time (Java)

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
